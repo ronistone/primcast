eb6715e70292cf730ada29ac1de3e76baae762a4
Leandro Pacheco <contact@leandropacheco.com>
2022-07-12 18:59:56 -0300

    removing nonsense comment from copy/pasting

--- primcast-net/examples/example.rs
+++ primcast-net/examples/example.rs
@@ -13,7 +13,6 @@ use rand::seq::SliceRandom;
 use rand::Rng;
 use std::time::Instant;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
