8cb289223bbe3cdb8d26e8e7c60377d2f8b0ccb6
Leandro Pacheco <contact@leandropacheco.com>
2022-07-16 17:19:24 -0300

    add merge() method to GidSet

--- primcast-core/src/types.rs
+++ primcast-core/src/types.rs
@@ -106,11 +106,18 @@ impl GidSet {
 
     // returns true if the element was inserted (false if already present)
     pub fn insert(&mut self, g: Gid) -> bool {
-        if self.0.iter().find(|it| **it == g).is_some() {
+        if self.contains(g) {
             false
         } else {
             self.0.push(g);
             true
+        }
+    }
+
+    // insert all entries from the other
+    pub fn merge(&mut self, other: &Self) {
+        for o in &other.0 {
+            self.insert(*o);
         }
     }
 
@@ -126,5 +133,37 @@ impl GidSet {
 
     pub fn contains(&self, g: Gid) -> bool {
         self.0.iter().find(|it| **it == g).is_some()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gidset() {
+        let mut s1 = GidSet::new();
+        let mut s2 = GidSet::new();
+
+        assert!(s1.is_empty());
+        assert_eq!(s1.len(), 0);
+
+        s1.insert(Gid(0));
+        s1.insert(Gid(1));
+        assert!(s1.contains(Gid(0)));
+        assert!(s1.contains(Gid(1)));
+        s2.insert(Gid(2));
+        s2.merge(&s1);
+        assert!(s2.contains(Gid(0)));
+        assert!(s2.contains(Gid(1)));
+        assert!(s2.contains(Gid(2)));
+        assert_eq!(s2.len(), 3);
+
+        s1.remove(Gid(0));
+        assert!(!s1.contains(Gid(0)));
+        assert_eq!(s1.len(), 1);
+
+        s1.clear();
+        assert!(s1.is_empty());
     }
 }
